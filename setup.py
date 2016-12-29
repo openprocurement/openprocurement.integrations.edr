b11e2671803c983e7f5c84b1d5251143199b897f
Krokop <chezar1995@gmail.com>
2016-12-29 11:42:26 +0200

    init repo

---
+++ setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+requires = [
+    'setuptools',
+]
+
+test_requires = requires + [
+    'webtest',
+    'python-coveralls',
+]
+
+docs_requires = requires + [
+    'sphinxcontrib-httpdomain',
+]
+
+databridge_requires = requires + [
+    'PyYAML',
+    'gevent',
+    'redis',
+    'LazyDB',
+    'ExtendedJournalHandler',
+    'openprocurement_client>=1.0b2'
+]
+
+entry_points = {
+    'console_scripts': [
+        'edr_data_bridge = openprocurement.integrations.edr.databridge:main'
+    ]
+}
+
+setup(name='openprocurement.integrations.edr',
+      version=version,
+      description="",
+      long_description=open("README.rst").read(),
+      classifiers=[
+        "Framework :: Pylons",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+        ],
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      url='https://github.com/Krokop/openprocurement.integrations.edr',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['openprocurement', 'openprocurement.tender'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=test_requires,
+      extras_require={'databridge': databridge_requires,
+                      'test': test_requires, 'docs': docs_requires},
+      entry_points=entry_points,
+      )
