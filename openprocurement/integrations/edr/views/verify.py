92af4916493745f257ccbbad905515e581c11e55
Ira <iryna.pomazan@unity-bars.com>
2017-02-01 10:46:03 +0200

    Implement request to EDR. Processing only response 200 and 400

--- openprocurement/integrations/edr/views/verify.py
+++ openprocurement/integrations/edr/views/verify.py
@@ -6,15 +6,25 @@ from openprocurement.integrations.edr.utils import opresource, APIResource
 
 
 @opresource(name='Verify customer',
-            path='/verify/{edrpo}',
+            path='/verify/{edrpou}',
             description="Verify customer by edr code ")
 class VerifyUResource(APIResource):
     """ Verify customer """
 
     @json_view(content_type="application/json", permission='verify')
     def get(self):
-        return {
-            'data': 'You are awesome {} {}'.format(
-                self.request.matchdict.get('edrpo'),
-                self.edr_api)
-        }
+        response = self.edr_api.get_subject(self.request.matchdict.get('edrpou'))
+        if response.status_code == 200:
+            data = response.json()
+            if not data:
+                self.request.errors.add('body', 'data', 'EDRPOU not found')
+                self.request.errors.status = 403
+                return
+            return {'data': data}
+        elif response.status_code == 400:
+            messages = [error['message'] for error in response.json()['errors']]
+            self.request.errors.add('body', 'data', 'Bad request. Message: {}'.format(messages))
+            self.request.errors.status = 403
+            return
+        #TODO all another errors realize
+
