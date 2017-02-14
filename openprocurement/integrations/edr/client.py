ee0814a7348a3c95b3acab98cb4d47fab544a80a
Ira <iryna.pomazan@unity-bars.com>
2017-02-14 11:20:50 +0200

    Added port, timeout in edr client

--- openprocurement/integrations/edr/client.py
+++ openprocurement/integrations/edr/client.py
@@ -1,22 +1,26 @@
+# -*- coding: utf-8 -*-
 import requests
 
 
 class EdrClient(object):
     """Base class for making requests to EDR"""
 
-    def __init__(self, server, token):
+    def __init__(self, server, token, timeout=None, port=80):
         self.token = token
-        self.url = '{server}/1.0/subjects'.format(server=server)
-        self.headers = {"Accept": "application/json", 'Authorization': 'Token {token}'.format(token=self.token)}
+        self.url = '{server}:{port}/1.0/subjects'.format(server=server, port=port)
+        self.headers = {"Accept": "application/json",
+                        "Authorization": "Token {token}".format(token=self.token)}
+        self.timeout = timeout
 
     def get_subject(self, code):
         """
-        Send request to EDR using EDRPOU (physical entity-entrepreneur) code or passport. In response we except list of
-        subjects with unique id in each subject. List mostly contains 1 subject, but occasionally includes 2 or none.
+        Send request to EDR using EDRPOU (physical entity-entrepreneur) code or passport.
+        In response we except list of subjects with unique id in each subject.
+        List mostly contains 1 subject, but occasionally includes 2 or none.
         """
-        param = 'code' if code.isdigit() and len(code) < 13 else 'passport'  # find out we accept edrpou or passport code
+        param = 'code' if code.isdigit() and (len(code) == 8 or len(code) == 10) else 'passport'  # find out we accept edrpou or passport code
         url = '{url}?{param}={code}'.format(url=self.url, param=param, code=code)
-        response = requests.get(url=url, headers=self.headers)
+        response = requests.get(url=url, headers=self.headers, timeout=self.timeout)
 
         return response
 
@@ -25,7 +29,7 @@ class EdrClient(object):
         Send request to ERD using unique identifier to get subject's details.
         """
         url = '{url}/{id}'.format(url=self.url, id=edr_unique_id)
-        response = requests.get(url=url, headers=self.headers)
+        response = requests.get(url=url, headers=self.headers, timeout=self.timeout)
 
         return response
 
