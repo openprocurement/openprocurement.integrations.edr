fb99f58c3873a8d052ea4703750cd47a95f813bf
dtrenkenshu <dtrenkenshu@olexander-zakharen.unity-bars.com.ua>
2017-04-18 14:40:42 +0300

    Added a basic unit test for /health

---
+++ openprocurement/integrations/edr/tests/health.py
@@ -0,0 +1,9 @@
+from openprocurement.integrations.edr.tests.base import BaseWebTest
+
+class TestHealth(BaseWebTest):
+    """ Test health view"""
+
+    def test_get(self):
+        response = self.app.get('/health')
+        self.assertEqual(response.status, "200 OK")
+
